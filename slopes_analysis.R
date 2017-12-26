a769298bc0bd7ecfbc4ab5cec6aed1a53a9c5ef6
Alfredo Hernández <aldomann.designs@gmail.com>
2017-12-26 18:44:04 +0100

    First commit

---
+++ slopes_analysis.R
@@ -0,0 +1,9 @@
+# Code to perform different statistical tests to analise the slopes of PDI vs duration
+# Author: Alfredo Hernández <aldomann.designs@gmail.com>
+
+
+# Libraries ------------------------------------------------
+library(measurements) # Convert units
+
+# Source base code -----------------------------------------
+
