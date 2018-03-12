33ffd83832ae21c9e31779dac636bb1e3180e011
Alfredo Hernández <aldomann.designs@gmail.com>
2018-03-12 12:58:28 +0100

    Reorganised and refactored code to updated functions

--- slopes_analysis.R
+++ slopes_analysis.R
@@ -19,6 +19,79 @@ pdi.epac <- pdi.all %>%
 	dplyr::filter(basin == "EPAC") %>%
 	dplyr::filter(storm.year >= 1986)
 
+
+# Confidence interval for all storms -----------------------
+
+# PDI ~ duration regression
+ci.yx.natl.low <- get_conf_interval(
+	pdi.natl, "low", "storm.duration", "storm.pdi")
+ci.yx.natl.high <- get_conf_interval(
+	pdi.natl, "high", "storm.duration", "storm.pdi")
+
+ci.yx.epac.low <- get_conf_interval(
+	pdi.epac, "low", "storm.duration", "storm.pdi")
+ci.yx.epac.high <- get_conf_interval(
+	pdi.epac, "high", "storm.duration", "storm.pdi")
+
+# Duration ~ PDI regression
+ci.xy.natl.low <- get_conf_interval(
+	pdi.natl, "low", "storm.pdi", "storm.duration")
+ci.xy.natl.high <- get_conf_interval(
+	pdi.natl, "high", "storm.pdi", "storm.duration")
+
+ci.xy.epac.low <- get_conf_interval(
+	pdi.epac, "low", "storm.pdi", "storm.duration")
+ci.xy.epac.high <- get_conf_interval(
+	pdi.epac, "high", "storm.pdi", "storm.duration")
+
+
+# Confidence interval for developing systems ---------------
+
+# PDI ~ duration regression
+ci.yx.natl.low.ds <- get_conf_interval(
+	pdi.natl, "low", "storm.duration", "storm.pdi", 33)
+ci.yx.natl.high.ds <- get_conf_interval(
+	pdi.natl, "high", "storm.duration", "storm.pdi", 33)
+
+ci.yx.epac.low.ds <- get_conf_interval(
+	pdi.epac, "low", "storm.duration", "storm.pdi", 33)
+ci.yx.epac.high.ds <- get_conf_interval(
+	pdi.epac, "high", "storm.duration", "storm.pdi", 33)
+
+# Duration ~ PDI regression
+ci.xy.natl.low.ds <- get_conf_interval(
+	pdi.natl, "low", "storm.pdi", "storm.duration", 33)
+ci.xy.natl.high.ds <- get_conf_interval(
+	pdi.natl, "high", "storm.pdi", "storm.duration", 33)
+
+ci.xy.epac.low.ds <- get_conf_interval(
+	pdi.epac, "low", "storm.pdi", "storm.duration", 33)
+ci.xy.epac.high.ds <- get_conf_interval(
+	pdi.epac, "high", "storm.pdi", "storm.duration", 33)
+
+
+# Permutation test for all data ----------------------------
+
+# PDI ~ duration regression
+p.val.yx.natl <- do_permutation_test(pdi.natl, "storm.pdi", "storm.duration")
+p.val.yx.epac <- do_permutation_test(pdi.epac, "storm.pdi", "storm.duration")
+
+# Duration ~ PDI regression
+p.val.xy.natl <- do_permutation_test(pdi.natl, "storm.duration", "storm.pdi")
+p.val.xy.epac <- do_permutation_test(pdi.epac, "storm.duration", "storm.pdi")
+
+
+# Permutation test for developing systems ------------------
+
+# PDI ~ duration regression
+p.val.yx.natl.ds <- do_permutation_test(pdi.natl, "storm.pdi", "storm.duration", 33)
+p.val.yx.epac.ds <- do_permutation_test(pdi.epac, "storm.pdi", "storm.duration", 33)
+
+# Duration ~ PDI regression
+p.val.xy.natl.ds <- do_permutation_test(pdi.natl, "storm.duration", "storm.pdi", 33)
+p.val.xy.epac.ds <- do_permutation_test(pdi.epac, "storm.duration", "storm.pdi", 33)
+
+
 # Scatterplots ---------------------------------------------
 
 plot_scatterplot(pdi.natl) + labs(title = "NATL") + theme_bw()
@@ -26,54 +99,3 @@ plot_scatterplot(pdi.epac) + labs(title = "EPAC") + theme_bw()
 
 plot_scatterplot(pdi.natl %>% dplyr::filter(max.wind > 33)) + labs(title = "NATL DS") + theme_bw()
 plot_scatterplot(pdi.epac %>% dplyr::filter(max.wind > 33)) + labs(title = "EPAC DS")+ theme_bw()
-
-# Confidence interval for y~x ------------------------------
-
-# ci.yx.natl.low <- get_conf_interval(pdi.natl, "low")
-# ci.yx.natl.high <- get_conf_interval(pdi.natl, "high")
-
-# ci.yx.epac.low <- get_conf_interval(pdi.epac, "low")
-# ci.yx.epac.high <- get_conf_interval(pdi.epac, "high")
-
-ci.yx.natl.low.ds <- get_conf_interval(pdi.natl %>% dplyr::filter(max.wind > 33), "low")
-ci.yx.natl.high.ds <- get_conf_interval(pdi.natl %>% dplyr::filter(max.wind > 33), "high")
-
-ci.yx.epac.low.ds <- get_conf_interval(pdi.epac %>% dplyr::filter(max.wind > 33), "low")
-ci.yx.epac.high.ds <- get_conf_interval(pdi.epac %>% dplyr::filter(max.wind > 33), "high")
-
-# Confidence interval for x~y ------------------------------
-
-# ci.xy.natl.low <- get_conf_interval(pdi.natl, "low", reg = "x~y")
-# ci.xy.natl.high <- get_conf_interval(pdi.natl, "high", reg = "x~y")
-
-# ci.xy.epac.low <- get_conf_interval(pdi.epac, "low", reg = "x~y")
-# ci.xy.epac.high <- get_conf_interval(pdi.epac, "high", reg = "x~y")
-
-ci.xy.natl.low.ds <- get_conf_interval(pdi.natl %>% dplyr::filter(max.wind > 33), "low", reg = "x~y")
-ci.xy.natl.high.ds <- get_conf_interval(pdi.natl %>% dplyr::filter(max.wind > 33), "high", reg = "x~y")
-
-ci.xy.epac.low.ds <- get_conf_interval(pdi.epac %>% dplyr::filter(max.wind > 33), "low", reg = "x~y")
-ci.xy.epac.high.ds <- get_conf_interval(pdi.epac %>% dplyr::filter(max.wind > 33), "high", reg = "x~y")
-
-
-# Permutation test for y~x ---------------------------------
-
-# All data
-p.val.yx.natl <- do_permutation_test(pdi.natl)
-p.val.yx.epac <- do_permutation_test(pdi.epac)
-
-# Developing systems
-p.val.yx.natl.ds <- do_permutation_test(pdi.natl %>% dplyr::filter(max.wind > 33))
-p.val.yx.epac.ds <- do_permutation_test(pdi.epac %>% dplyr::filter(max.wind > 33))
-
-
-# Permutation test for x~y ---------------------------------
-
-# All data
-p.val.xy.natl <- do_permutation_test(pdi.natl, reg = "x~y")
-p.val.xy.epac <- do_permutation_test(pdi.epac, reg = "x~y")
-
-# Developing systems
-p.val.xy.natl.ds <- do_permutation_test(pdi.natl %>% dplyr::filter(max.wind > 33), reg = "x~y")
-p.val.xy.epac.ds <- do_permutation_test(pdi.epac %>% dplyr::filter(max.wind > 33), reg = "x~y")
-
