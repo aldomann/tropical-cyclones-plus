612ecd14e24939f747fd0c59ebcb7320bb47376c
Alfredo Hernández <aldomann.designs@gmail.com>
2018-04-17 16:55:52 +0200

    Stopped loading data.table for fread()

--- slopes_base.R
+++ slopes_base.R
@@ -4,7 +4,6 @@
 # Libraries ------------------------------------------------
 library(tidyverse)
 library(lubridate)
-library(data.table)
 
 # Confidence interval --------------------------------------
 
