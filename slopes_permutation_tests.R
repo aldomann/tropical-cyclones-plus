ade3e7568ab219910959b0ec490182b197b9825f
Alfredo Hernández <aldomann.designs@gmail.com>
2018-04-17 09:07:00 +0200

    Include compute.flag

--- slopes_permutation_tests.R
+++ slopes_permutation_tests.R
@@ -7,7 +7,7 @@ library(measurements) # Convert units
 
 # Source base code -----------------------------------------
 source("slopes_base.R")
-# load("slopes_analysis.RData")
+load("slopes_analysis.RData")
 
 # Get RAW data ---------------------------------------------
 
@@ -21,38 +21,47 @@ pdi.epac <- pdi.all %>%
 
 # Permutation test for all data ----------------------------
 
-# NATL
-p.vals.natl.pdi <- summarise_p_values("NATL", "storm.duration", "storm.pdi")
-p.vals.natl.max.wind <- summarise_p_values("NATL", "storm.duration", "max.wind")
-p.vals.natl.mean.wind <- summarise_p_values("NATL", "storm.duration", "mean.wind")
-p.vals.natl.mean.sq.wind <- summarise_p_values("NATL", "storm.duration", "mean.sq.wind")
+compute.flag <- FALSE
 
-# EPAC
-p.vals.epac.pdi <- summarise_p_values("EPAC", "storm.duration", "storm.pdi")
-p.vals.epac.max.wind <- summarise_p_values("EPAC", "storm.duration", "max.wind")
-p.vals.epac.mean.wind <- summarise_p_values("EPAC", "storm.duration", "mean.wind")
-p.vals.epac.mean.sq.wind <- summarise_p_values("EPAC", "storm.duration", "mean.sq.wind")
+if (compute.flag) {
+	# NATL
+	p.vals.natl.pdi <- summarise_p_values("NATL", "storm.duration", "storm.pdi")
+	p.vals.natl.max.wind <- summarise_p_values("NATL", "storm.duration", "max.wind")
+	p.vals.natl.mean.wind <- summarise_p_values("NATL", "storm.duration", "mean.wind")
+	p.vals.natl.mean.sq.wind <- summarise_p_values("NATL", "storm.duration", "mean.sq.wind")
 
-# Permutation test for developing systems ------------------
+	# EPAC
+	p.vals.epac.pdi <- summarise_p_values("EPAC", "storm.duration", "storm.pdi")
+	p.vals.epac.max.wind <- summarise_p_values("EPAC", "storm.duration", "max.wind")
+	p.vals.epac.mean.wind <- summarise_p_values("EPAC", "storm.duration", "mean.wind")
+	p.vals.epac.mean.sq.wind <- summarise_p_values("EPAC", "storm.duration", "mean.sq.wind")
 
-# NATL
-p.vals.natl.pdi.ds <- summarise_p_values("NATL", "storm.duration", "storm.pdi", 33)
-p.vals.natl.max.wind.ds <- summarise_p_values("NATL", "storm.duration", "max.wind", 33)
-p.vals.natl.mean.wind.ds <- summarise_p_values("NATL", "storm.duration", "mean.wind", 33)
-p.vals.natl.mean.sq.wind.ds <- summarise_p_values("NATL", "storm.duration", "mean.sq.wind", 33)
+	# Permutation test for developing systems ------------------
 
-# EPAC
-p.vals.epac.pdi.ds <- summarise_p_values("EPAC", "storm.duration", "storm.pdi", 33)
-p.vals.epac.max.wind.ds <- summarise_p_values("EPAC", "storm.duration", "max.wind", 33)
-p.vals.epac.mean.wind.ds <- summarise_p_values("EPAC", "storm.duration", "mean.wind", 33)
-p.vals.epac.mean.sq.wind.ds <- summarise_p_values("EPAC", "storm.duration", "mean.sq.wind", 33)
+	# NATL
+	p.vals.natl.pdi.ds <- summarise_p_values("NATL", "storm.duration", "storm.pdi", 33)
+	p.vals.natl.max.wind.ds <- summarise_p_values("NATL", "storm.duration", "max.wind", 33)
+	p.vals.natl.mean.wind.ds <- summarise_p_values("NATL", "storm.duration", "mean.wind", 33)
+	p.vals.natl.mean.sq.wind.ds <- summarise_p_values("NATL", "storm.duration", "mean.sq.wind", 33)
+
+	# EPAC
+	p.vals.epac.pdi.ds <- summarise_p_values("EPAC", "storm.duration", "storm.pdi", 33)
+	p.vals.epac.max.wind.ds <- summarise_p_values("EPAC", "storm.duration", "max.wind", 33)
+	p.vals.epac.mean.wind.ds <- summarise_p_values("EPAC", "storm.duration", "mean.wind", 33)
+	p.vals.epac.mean.sq.wind.ds <- summarise_p_values("EPAC", "storm.duration", "mean.sq.wind", 33)
+}
+
+# Tidy p-values in a list ----------------------------------
+
+if (compute.flag) {
+	print("hello")
+	# Group data frames into a list
+	rm(p.values.list)
+	p.values.list <- lapply(ls(patt='^p.vals.'), get)
+	# rm(list=ls(pattern="^p.vals."))
+}
 
 # Analyse p-values -----------------------------------------
-
-# Group data frames into a list
-rm(p.values.list)
-p.values.list <- lapply(ls(patt='^p.vals.'), get)
-# rm(list=ls(pattern="^p.vals."))
 
 alpha = 0.05
 
