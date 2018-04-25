85c3e5b47ca4634f73d1b1d901573c943c9a8170
Alfredo Hernández <aldomann.designs@gmail.com>
2018-04-25 23:49:37 +0200

    Added max.wind ~ storm.duration boot+perm objects

--- slopes_permutation_tests.R
+++ slopes_permutation_tests.R
@@ -7,6 +7,7 @@ source("slopes_base.R")
 # load("slopes_analysis.RData")
 
 zug_zug <- function(call) {
+	print(paste("Started computation at", Sys.time()))
 	print(system.time(call))
 	system("notify-send 'Finished calculations' 'Get back to work!' -i rstudio -u critical")
 	system("paplay /usr/share/sounds/freedesktop/stereo/complete.oga")
@@ -87,13 +88,13 @@ if (compute.flag && bootstrap.flag) {
 
 	# NATL
 	boot.p.vals.natl.pdi <- summarise_p_values("NATL", "storm.duration", "storm.pdi", bootstrap = T, n.sim = 500)
-	# boot.p.vals.natl.max.wind <- summarise_p_values("NATL", "storm.duration", "max.wind", bootstrap = T, n.sim = 500)
+	zug_zug(boot.p.vals.natl.max.wind <- summarise_p_values("NATL", "storm.duration", "max.wind", bootstrap = T, n.sim = 500))
 	# boot.p.vals.natl.mean.wind <- summarise_p_values("NATL", "storm.duration", "mean.wind", bootstrap = T, n.sim = 500)
 	# boot.p.vals.natl.mean.sq.wind <- summarise_p_values("NATL", "storm.duration", "mean.sq.wind", bootstrap = T, n.sim = 500)
 
 	# EPAC
 	boot.p.vals.epac.pdi <- summarise_p_values("EPAC", "storm.duration", "storm.pdi", bootstrap = T, n.sim = 500)
-	# boot.p.vals.epac.max.wind <- summarise_p_values("EPAC", "storm.duration", "max.wind", bootstrap = T, n.sim = 500)
+	zug_zug(boot.p.vals.epac.max.wind <- summarise_p_values("EPAC", "storm.duration", "max.wind", bootstrap = T, n.sim = 500))
 	# boot.p.vals.epac.mean.wind <- summarise_p_values("EPAC", "storm.duration", "mean.wind", bootstrap = T, n.sim = 500)
 	# boot.p.vals.epac.mean.sq.wind <- summarise_p_values("EPAC", "storm.duration", "mean.sq.wind", bootstrap = T, n.sim = 500)
 }
@@ -102,13 +103,13 @@ if (compute.flag && bootstrap.flag) {
 if (compute.flag && bootstrap.flag) {
 	# NATL
 	boot.p.vals.natl.pdi.ds <- summarise_p_values("NATL", "storm.duration", "storm.pdi", 33, bootstrap = T, n.sim = 500)
-	# boot.p.vals.natl.max.wind.ds <- summarise_p_values("NATL", "storm.duration", "max.wind", 33, bootstrap = T, n.sim = 500)
+	zug_zug(boot.p.vals.natl.max.wind.ds <- summarise_p_values("NATL", "storm.duration", "max.wind", 33, bootstrap = T, n.sim = 500))
 	# boot.p.vals.natl.mean.wind.ds <- summarise_p_values("NATL", "storm.duration", "mean.wind", 33, bootstrap = T, n.sim = 500)
 	# boot.p.vals.natl.mean.sq.wind.ds <- summarise_p_values("NATL", "storm.duration", "mean.sq.wind", 33, bootstrap = T, n.sim = 500)
 
 	# EPAC
 	boot.p.vals.epac.pdi.ds <- summarise_p_values("EPAC", "storm.duration", "storm.pdi", 33, bootstrap = T, n.sim = 500)
-	# boot.p.vals.epac.max.wind.ds <- summarise_p_values("EPAC", "storm.duration", "max.wind", 33, bootstrap = T, n.sim = 500)
+	zug_zug(boot.p.vals.epac.max.wind.ds <- summarise_p_values("EPAC", "storm.duration", "max.wind", 33, bootstrap = T, n.sim = 500))
 	# boot.p.vals.epac.mean.wind.ds <- summarise_p_values("EPAC", "storm.duration", "mean.wind", 33, bootstrap = T, n.sim = 500)
 	# boot.p.vals.epac.mean.sq.wind.ds <- summarise_p_values("EPAC", "storm.duration", "mean.sq.wind", 33, bootstrap = T, n.sim = 500)
 
@@ -129,17 +130,28 @@ if (compute.flag && !bootstrap.flag) {
 	rm(p.values.list)
 	p.values.list <- lapply(ls(patt='^p.vals.'), get)
 	p.values.list.pdi <- lapply(ls(patt='^p.vals.*pdi*'), get)
+	p.values.list.max.wind <- lapply(ls(patt='^p.vals.*max.wind*'), get)
+
 	# rm(list=ls(pattern="^p.vals."))
+
 	saveRDS(p.values.list, "slopes_p_values.rds")
 	saveRDS(p.values.list.pdi, "slopes_p_values_pdi.rds")
+	saveRDS(p.values.list.max.wind, "slopes_p_values_maxwind.rds")
 }
 
 if (compute.flag && bootstrap.flag) {
 	# Group data frames into a list
 	rm(boot.p.values.list)
+
 	boot.p.values.list <- lapply(ls(patt='^boot.p.vals.'), get)
+	boot.p.values.list.pdi <- lapply(ls(patt='^boot.p.vals.*pdi*'), get)
+	boot.p.values.list.max.wind <- lapply(ls(patt='^boot.p.vals.*max.wind*'), get)
+
 	# rm(list=ls(pattern="^boot.p.vals."))
+
 	saveRDS(boot.p.values.list, "slopes_p_values_boot.rds")
+	saveRDS(boot.p.values.list.pdi, "slopes_p_values_boot_pdi.rds")
+	saveRDS(boot.p.values.list.max.wind, "slopes_p_values_boot_maxwind.rds")
 }
 
 # Analyse p-values -----------------------------------------
@@ -147,7 +159,11 @@ if (compute.flag && bootstrap.flag) {
 # Print regressions with p-value <= alpha
 explore_p_values(p.values.list, 0.05)
 explore_p_values(p.values.list.pdi, 0.05)
+explore_p_values(p.values.list.max.wind, 0.05)
+
 explore_p_values(boot.p.values.list, 0.05)
+explore_p_values(boot.p.values.list.pdi, 0.05)
+explore_p_values(boot.p.values.list.max.wind, 0.05)
 
 # NATL (all storms)
 p.values.list[lapply(purrr::map(p.values.list, ~dplyr::filter(.x, basin == "NATL", min.speed == 0)), nrow) > 0][[4]]
