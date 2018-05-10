af3ac98937809d1697a1ca9eafa8050872e6cb79
Alfredo Hernández <aldomann.designs@gmail.com>
2018-05-11 00:26:31 +0200

    Bivariate lognormal simulation

---
+++ slopes_sim.R
@@ -0,0 +1,62 @@
+# Base code to perform different statistical tests to analise the slopes of PDI vs duration
+# Author: Alfredo Hernández <aldomann.designs@gmail.com>
+
+
+# Source base code -----------------------------------------
+source("slopes_base.R")
+source("slopes_sim_base.R")
+
+# Get RAW data ---------------------------------------------
+
+pdi.all <- as_tibble(data.table::fread('data/hurdat2-hadisst-1966-2016_pdis.csv')) %>%
+	mutate(storm.duration = measurements::conv_unit(storm.duration, "sec", "hr"))
+
+pdi.natl <- pdi.all %>%
+	dplyr::filter(basin == "NATL")
+pdi.epac <- pdi.all %>%
+	dplyr::filter(basin == "EPAC")
+
+# Simulate data from data ----------------------------------
+
+pdi.natl.small <- pdi.natl %>%
+	dplyr::filter(sst.class == "high") %>%
+	dplyr::select(storm.pdi, storm.duration)
+
+sim.pdi.natl <- simulate_bvlnd(pdi.natl.small)
+
+# Testing the simulation
+ggplot(sim.pdi.natl) +
+	geom_point(aes(x = V1, y = V2), colour = "red") +
+	geom_point(data = pdi.natl,
+						 aes(x = storm.pdi, y = storm.duration), colour = "blue") +
+	scale_x_log10() +
+	scale_y_log10()
+
+
+# NATL simulation ------------------------------------------
+
+pdi.natl.sim <- simulate_hurricane_obs("NATL", "storm.pdi", "storm.duration", 33)
+
+ggplot(pdi.natl.sim) +
+	geom_point(aes(x = storm.pdi, y = storm.duration, colour = sst.class)) +
+	scale_x_log10(limits = c(1e8, 1e12)) +
+	scale_y_log10(limits = c(25, 1000)) +
+	labs(title = "Simulated data (NATL, developing systems)") +
+	theme_bw()
+
+ggplot(pdi.natl %>% dplyr::filter(max.wind > 33)) +
+	geom_point(aes(x = storm.pdi, y = storm.duration, colour = sst.class)) +
+	scale_x_log10(limits = c(1e8, 1e12)) +
+	scale_y_log10(limits = c(25, 1000)) +
+	labs(title = "Real data (NATL, developing systems)") +
+	theme_bw()
+
+# Estimate confidence intervals
+(ci.natl.pdi.ds <- summarise_conf_intervals("NATL", "storm.duration", "storm.pdi", 33))
+(ci.natl.sim.pdi.ds <- summarise_conf_intervals("NATL", "storm.duration", "storm.pdi", 33))
+
+# Perform permutation tests
+n.sim.test <- 1000
+p.vals.natl.pdi.ds.std <- summarise_p_values("NATL", "storm.duration", "storm.pdi", 33, F, n.sim.test)
+p.vals.natl.pdi.ds.boot <- summarise_p_values("NATL", "storm.duration", "storm.pdi", 33, T, n.sim.test)
+
---
+++ slopes_sim_base.R
@@ -0,0 +1,85 @@
+# Base code to perform different statistical tests to analise the slopes of PDI vs duration
+# Author: Alfredo Hernández <aldomann.designs@gmail.com>
+
+
+
+# Generic bivariate simulation functions -------------------
+
+estimate_bvnd <- function(df, n.samp) {
+	# Mu
+	means <- colMeans(df)
+	# Sigma
+	cov.matrix <- var(df)
+
+	params <- tibble(
+		mu.x = means[1],
+		mu.y = means[2],
+		sigma1 = cov.matrix[1,1],
+		sigma2 = cov.matrix[2,2],
+		rho = cov.matrix[1,2]
+	)
+
+	return(params)
+}
+
+simulate_bvnd <- function(df) {
+	ests <- estimate_bvnd(df)
+	n.samp <- nrow(df)
+
+	means <- c(ests[[1]], ests[[2]])
+
+	cov.matrix <- matrix(c(ests[[3]], ests[[5]],
+												 ests[[5]], ests[[4]]),
+											 2)
+
+	model <- MASS::mvrnorm(n.samp, mu = means, Sigma = cov.matrix)
+
+	return(as_tibble(model))
+}
+
+simulate_bvlnd <- function(df) {
+	df <- log10(df)
+	ests <- estimate_bvnd(df)
+	n.samp <- nrow(df)
+
+	means <- c(ests[[1]], ests[[2]])
+
+	cov.matrix <- matrix(c(ests[[3]], ests[[5]],
+												 ests[[5]], ests[[4]]),
+											 2)
+
+	model <- MASS::mvrnorm(n.samp, mu = means, Sigma = cov.matrix)
+
+	return(as_tibble(10^model))
+}
+
+
+# Hurricane simulation functions ---------------------------
+
+simulate_hurricane_obs <- function(basin.name, var1, var2, min.speed) {
+	# Filter and clean data
+	df <- pdi.all %>%
+		dplyr::filter(basin == basin.name) %>%
+		dplyr::filter(max.wind > min.speed) %>%
+		dplyr::select(var1, var2, sst.class)
+
+	df.low <- df %>%
+		dplyr::filter(sst.class == "low") %>%
+		select(-sst.class)
+
+	df.high <- df %>%
+		dplyr::filter(sst.class == "high") %>%
+		select(-sst.class)
+
+	# Simulate the data
+	sim.df.low <- simulate_bvlnd(df.low) %>%
+		mutate(sst.class = "low") %>%
+		rename_(.dots = setNames(c("V1", "V2"),
+														 c(var1, var2)))
+	sim.df.high <- simulate_bvlnd(df.high) %>%
+		mutate(sst.class = "high") %>%
+		rename_(.dots = setNames(c("V1", "V2"),
+														 c(var1, var2)))
+
+	return(rbind(sim.df.low, sim.df.high))
+}
