7275f63d3d9d21e290f31bebfeddaddce54538b5
Alfredo Hernández <aldomann.designs@gmail.com>
2018-06-29 06:41:01 +0200

    Included results for EPAC

--- residuals_analysis.R
+++ residuals_analysis.R
@@ -4,6 +4,7 @@
 
 # Source base code -----------------------------------------
 source("regression_base.R")
+source("resampling_base.R")
 source("residuals_base.R")
 
 # Get RAW data ---------------------------------------------
@@ -18,54 +19,126 @@ pdi.epac <- pdi.all %>%
 
 # Clean data -----------------------------------------------
 
-data.low <- pdi.natl %>%
+natl.data.low <- pdi.natl %>%
 	dplyr::filter(sst.class == "low") %>%
 	dplyr::filter(max.wind > 33)
 
-data.high <- pdi.natl %>%
+natl.data.high <- pdi.natl %>%
+	dplyr::filter(sst.class == "high") %>%
+	dplyr::filter(max.wind > 33)
+
+epac.data.low <- pdi.epac %>%
+	dplyr::filter(sst.class == "low") %>%
+	dplyr::filter(max.wind > 33)
+
+epac.data.high <- pdi.epac %>%
 	dplyr::filter(sst.class == "high") %>%
 	dplyr::filter(max.wind > 33)
 
 
 # Linear models --------------------------------------------
 
-fit.low <- lm(data = data.low, log10(storm.pdi) ~ log10(storm.duration))
-fit.high <- lm(data = data.high, log10(storm.pdi) ~ log10(storm.duration))
+natl.fit.low <- lm(data = natl.data.low, log10(storm.pdi) ~ log10(storm.duration))
+natl.fit.high <- lm(data = natl.data.high, log10(storm.pdi) ~ log10(storm.duration))
 
-boot.low <- perform_bootstrap(data.low$storm.duration, data.low$storm.pdi, 5000)
-boot.low.stats <- boot.low[[1]]
-boot.low.data <- boot.low[[2]]
+epac.fit.low <- lm(data = epac.data.low, log10(storm.pdi) ~ log10(storm.duration))
+epac.fit.high <- lm(data = epac.data.high, log10(storm.pdi) ~ log10(storm.duration))
 
-boot.high <- perform_bootstrap(data.high$storm.duration, data.high$storm.pdi, 5000)
-boot.high.stats <- boot.high[[1]]
-boot.high.data <- boot.high[[2]]
+set.seed(1)
+natl.boot.low <- perform_bootstrap(natl.data.low$storm.duration, natl.data.low$storm.pdi, 5000)
+natl.boot.low.stats <- natl.boot.low[[1]]
+natl.boot.low.data <- natl.boot.low[[2]]
 
+set.seed(1)
+natl.boot.high <- perform_bootstrap(natl.data.high$storm.duration, natl.data.high$storm.pdi, 5000)
+natl.boot.high.stats <- natl.boot.high[[1]]
+natl.boot.high.data <- natl.boot.high[[2]]
 
-# Histograms of sample -------------------------------------
-ggplot(boot.low.data) +
-	geom_histogram(aes(x = slope), binwidth = 0.01)
+set.seed(1)
+epac.boot.low <- perform_bootstrap(epac.data.low$storm.duration, epac.data.low$storm.pdi, 5000)
+epac.boot.low.stats <- epac.boot.low[[1]]
+epac.boot.low.data <- epac.boot.low[[2]]
 
-ggplot(boot.low.data) +
-	geom_histogram(aes(x = inter), binwidth = 0.025)
-
-ggplot(boot.low.data) +
-	geom_histogram(aes(x = r2), binwidth = 0.005)
+set.seed(1)
+epac.boot.high <- perform_bootstrap(epac.data.high$storm.duration, epac.data.high$storm.pdi, 5000)
+epac.boot.high.stats <- epac.boot.high[[1]]
+epac.boot.high.data <- epac.boot.high[[2]]
 
 
 # Residuals vs fitted values -------------------------------
-plot_resid_vs_fitted(fit.low)
-plot_resid_vs_fitted(fit.high)
+plot_resid_vs_fitted(natl.fit.low) #+ theme(text = element_text(family = "Palatino")) + ggsave(filename = "natl_low_resid_fitted.pdf", width = 4, height = 2.5, dpi = 300, device = cairo_pdf)
+plot_resid_vs_fitted(natl.fit.high) #+ theme(text = element_text(family = "Palatino")) + ggsave(filename = "natl_high_resid_fitted.pdf", width = 4, height = 2.5, dpi = 300, device = cairo_pdf)
+
+plot_resid_vs_fitted(epac.fit.low) #+ theme(text = element_text(family = "Palatino")) + ggsave(filename = "epac_low_resid_fitted.pdf", width = 4, height = 2.5, dpi = 300, device = cairo_pdf)
+plot_resid_vs_fitted(epac.fit.high) #+ theme(text = element_text(family = "Palatino")) + ggsave(filename = "epac_high_resid_fitted.pdf", width = 4, height = 2.5, dpi = 300, device = cairo_pdf)
 
 
 # QQ Plots (residuals) -------------------------------------
-plot_resid_qqplot(fit.low)
-plot_resid_qqplot(fit.high)
+plot_resid_qqplot(natl.fit.low) #+ theme(text = element_text(family = "Palatino")) + ggsave(filename = "natl_low_resid_qqplot.pdf", width = 4, height = 2.5, dpi = 300, device = cairo_pdf)
+plot_resid_qqplot(natl.fit.high) #+ theme(text = element_text(family = "Palatino")) + ggsave(filename = "natl_high_resid_qqplot.pdf", width = 4, height = 2.5, dpi = 300, device = cairo_pdf)
 
-boot.low.resid <- get_boot_residuals(data.low$storm.duration, data.low$storm.pdi, boot.low.stats)
+plot_resid_qqplot(epac.fit.low) #+ theme(text = element_text(family = "Palatino")) + ggsave(filename = "epac_low_resid_qqplot.pdf", width = 4, height = 2.5, dpi = 300, device = cairo_pdf)
+plot_resid_qqplot(epac.fit.high) #+ theme(text = element_text(family = "Palatino")) + ggsave(filename = "epac_high_resid_qqplot.pdf", width = 4, height = 2.5, dpi = 300, device = cairo_pdf)
 
-plot_qqplot(boot.low.resid[["resid"]], "Residuals")
+
+# Histograms of sample -------------------------------------
+plot_boot_coefs_hist <- function(data.low, data.high, stat) {
+	all.data <- rbind(
+		cbind(data.low, sst.class = "low"),
+		cbind(data.high, sst.class = "high")
+	)
+
+	ggplot(all.data) +
+		aes_string(x = stat) +
+		geom_histogram(aes(fill = sst.class, group = sst.class),
+									 position = "identity", colour = "black", bins = 50, alpha = 0.5) +
+		scale_fill_manual(values = c("high" = "brown1",
+																 "low"  = "dodgerblue1")) +
+		labs(y = "Count", x = paste("Bootstraped", eval(stat)), fill = "SST Class" ) +
+		theme_bw()
+}
+
+# ggplot(natl.boot.low.data) +
+# 	geom_histogram(aes(x = r2), binwidth = 0.005)
+
+plot_boot_coefs_hist(natl.boot.low.data, natl.boot.high.data, "slope")# + theme(text = element_text(family = "Palatino")) + ggsave(filename = "natl_boot_slope.pdf", width = 4, height = 3, dpi = 300, device = cairo_pdf)
+plot_dists_qqplot(natl.boot.low.data$slope, natl.boot.high.data$slope)
+
+plot_boot_coefs_hist(natl.boot.low.data, natl.boot.high.data, "inter")# + theme(text = element_text(family = "Palatino")) + ggsave(filename = "natl_boot_inter.pdf", width = 4, height = 3, dpi = 300, device = cairo_pdf)
+plot_dists_qqplot(natl.boot.low.data$inter, natl.boot.high.data$inter)
+
+plot_boot_coefs_hist(epac.boot.low.data, epac.boot.high.data, "slope")# + theme(text = element_text(family = "Palatino")) + ggsave(filename = "epac_boot_slope.pdf", width = 4, height = 3, dpi = 300, device = cairo_pdf)
+plot_dists_qqplot(epac.boot.low.data$slope, epac.boot.high.data$slope)
+
+plot_boot_coefs_hist(epac.boot.low.data, epac.boot.high.data, "inter")# + theme(text = element_text(family = "Palatino")) + ggsave(filename = "epac_boot_inter.pdf", width = 4, height = 3, dpi = 300, device = cairo_pdf)
+plot_dists_qqplot(epac.boot.low.data$inter, epac.boot.high.data$inter)
+
+
+
+# Residual tests -------------------------------------------
+
+perform_fit_residual_tests(natl.fit.low)
+perform_fit_residual_tests(natl.fit.high)
+
+perform_fit_residual_tests(epac.fit.low)
+perform_fit_residual_tests(epac.fit.high)
+
+
+# QQ Plots with bootstrap (residuals) ----------------------
+
+natl.boot.low.resid <- get_boot_residuals(natl.data.low$storm.duration, natl.data.low$storm.pdi, natl.boot.low.stats)
+plot_normal_qqplot(natl.boot.low.resid[["resid"]], "Residuals")
 
 
 # Q-Q Plots (any variable) ---------------------------------
-plot_qqplot(boot.low.data[["slope"]], "Slope")
-plot_qqplot(boot.low.data[["inter"]], "Intercept")
+plot_normal_qqplot(natl.boot.low.data[["slope"]], "Slope")
+plot_normal_qqplot(natl.boot.high.data[["slope"]], "Slope")
+
+plot_normal_qqplot(natl.boot.low.data[["inter"]], "Intercept")
+plot_normal_qqplot(natl.boot.high.data[["inter"]], "Intercept")
+
+plot_normal_qqplot(epac.boot.low.data[["slope"]], "Slope")
+plot_normal_qqplot(epac.boot.high.data[["slope"]], "Slope")
+
+plot_normal_qqplot(epac.boot.low.data[["inter"]], "Intercept")
+plot_normal_qqplot(epac.boot.high.data[["inter"]], "Intercept")
